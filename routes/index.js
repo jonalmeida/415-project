1d9b2ec7ff4b06dc91762b468d44245689f3194c
Jonathan Almeida <git@jonalmeida.com>
2014-12-11 02:47:41 -0500

    Boilerplate code and templates

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  var data_code = fs.readFileSync('routes/index.js', "utf-8");
+
+  //   , function (err, data) {
+  //   if (err) {
+  //     return err;
+  //   }
+  //   //console.log(data);
+  //   // res.send(data);
+  //   // data_code = data;
+    
+  //   // res.render('index', { 
+  //   //   title: 'Cosine Similarity Measure',
+  //   //   dataCode: data
+  //   // });
+
+  //   res.use(function(req, res, next){
+  //     res.locals.dataCode = req.data;
+  //     next();
+  //   });
+  // });
+
+  res.render('index', { 
+    title: 'Cosine Similarity Measure',
+    dataCode: data_code
+  });
+
+});
+
+module.exports = router;
